9bac143db46814cb6db2f2a85c7b03617300f84b
jiaoyuan <y4096@outlook.com>
2023-02-26 22:23:55 +0800

    deploy configuration

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-h41$kcyv)*%_qdut=$5*28)=mzx5frslhxtcleu-h8x2kxd&%=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
